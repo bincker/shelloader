af82432215b473f8694115d337853196ca7a8e00
rjkall <rj@notme.com>
2013-03-08 13:51:54 -0800

    added GPL, verbosity

--- shelloader.c
+++ shelloader.c
@@ -1,9 +1,29 @@
 /* shelloader.c - Linux 64-Bit mmap based shell code loader
+ *-------------------------------------------------------------------------
  * pass it your object file and it will display and execute the shellcode
  * date 3/8/2013
  * author Travis "rjkall"
+ *-------------------------------------------------------------------------
+ * http://www.blackhatlibrary.net
  *
- * NOTE: Still buggy, work in progress.
+ * UPDATES:
+ * - Added more verbose output for user
+ * - Added null byte warning
+ *------------------------------------------------------------------------
+ *  (C) Copyright 2013 Travis "rjkall"
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,7 +65,7 @@ int parse(char *obj_file) {
 			printf("[*] %s is not a valid ELF object file!\n",obj_file);
 			return -1;
 		} else {
-			printf("[*] EI_MAG0 = 0x7f, continuing...\n");
+			printf("[*] EI_MAG0 = 0x7f, continuing.\n");
 		}
 				
 		shdr = (Elf64_Shdr *)malloc(sizeof(shdr));
@@ -60,7 +80,8 @@ int parse(char *obj_file) {
 		*/
 		fseek(obj,ehdr.e_shoff,SEEK_SET);
 		fread(shdr, sizeof(*shdr), ehdr.e_shnum, obj);
-		while(sections++ < ehdr.e_shnum) {		
+		
+		while(sections++ < ehdr.e_shnum) {			
 			fseek(obj,shdr[ehdr.e_shstrndx].sh_offset+shdr[sections].sh_name,SEEK_SET);
 			fgets(sname,6,obj);
 			if((strncmp(sname,".text",5)) == 0) {
@@ -70,8 +91,8 @@ int parse(char *obj_file) {
 				*/
 				addr = shdr[sections].sh_offset;
 				addrlen = shdr[sections].sh_size;
-				printf("[*] Found .text section at address 0x%08x with length of %d bytes\n",addr,addrlen);
-				printf("[*] Dumping shellcode...\n");
+				printf("[*] Found .text section at address 0x%08x with length of %d bytes.\n",addr,addrlen);
+				printf("[*] Dumping shellcode.\n");
 		
 				/*
 				 * sh_offset is the offset of the section data from the beginning
@@ -120,11 +141,11 @@ int parse(char *obj_file) {
 int executecode(unsigned char *exshellcode, int shellen) {
 	unsigned char *shellcode;
 
-	printf("\n[*] Mapping and copying shellcode to memory...\n");	
+	printf("\n[*] Mapping and copying %d bytes of shellcode to memory.\n",shellen);	
 	shellcode = (unsigned char *)mmap(0,shellen-1,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
 	memcpy(shellcode, exshellcode,shellen);
 	
-	printf("[*] Executing %d bytes of shellcode at %p...\n",strlen(shellcode),shellcode);
+	printf("[*] Executing shellcode at address %p.\n",shellcode);
 	(*(void(*)()) shellcode)();
 
 	return 0;
