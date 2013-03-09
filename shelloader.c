3b9305eef5e237b15e7c9821d3908f7ae07b1892
rjkall <rj@notme.com>
2013-03-08 16:50:57 -0800

    fixed various things

--- shelloader.c
+++ shelloader.c
@@ -31,32 +31,23 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <elf.h>
-#include <assert.h>
 
-/* 
- * This function parses the object file, displays the shellcode and then sends it to execute
- * it is sort of messy, I may clean it up later.
-*/
 int parse(char *obj_file,int exec) {
-	/* ELF variables */
 	FILE *obj;
 	Elf64_Ehdr ehdr;
 	Elf64_Shdr *shdr;
-
-	/* Misc variables for data manipulation */
 	char sname[6];
 	int sections = 0;
-	int addrlen = 0;
-	int addr = 0;
-	int i = 0;
+	int addrlen  = 0;
+	int addr     = 0;
 	int nullcntr = 0;
+	int i        = 0;
 	
 	if((obj=fopen(obj_file,"r+b")) == NULL) {
 		printf("[*] Unable to open %s! Quitting.\n",obj_file);
 		return -1;
 	} else {
 		/*
-		 * First step is to check that this is a valid ELF object file
 		 * I only check EI_MAG0 not EI_MAG0-3, I figured it is good
                  * enough for this purpose.
 		*/
@@ -87,9 +78,6 @@ int parse(char *obj_file,int exec) {
 			fgets(sname,6,obj);
 			if((strncmp(sname,".text",5)) == 0) {
 
-				/*
-				 * This just makes it less messy but still to long of string
-				*/
 				addr = shdr[sections].sh_offset;
 				addrlen = shdr[sections].sh_size;
 				printf("[*] Found .text section at address 0x%08x with length of %d bytes.\n",addr,addrlen);
@@ -117,16 +105,10 @@ int parse(char *obj_file,int exec) {
 				printf("\n");
 				close(obj);	
 				
-				/*
-				 * If null bytes were detected warn user
-				*/
 				if(nullcntr > 0) {
 					printf("[*] WARNING: Detected %d null bytes!\n",nullcntr);
 				}
 
-				/*
-				 * Finally execute the byte code
-				*/
 				if(exec == 1) {
 					executecode(obj_data,addrlen);
 				}
@@ -138,10 +120,6 @@ int parse(char *obj_file,int exec) {
 	return 0;
 }
 
-/*
- * copy shellcode to memory we have mapped and execute it
- * -has been known to be a bit buggy on some systems
-*/
 int executecode(unsigned char *exshellcode, int shellen) {
 	unsigned char *shellcode;
 
@@ -155,17 +133,10 @@ int executecode(unsigned char *exshellcode, int shellen) {
 	return 0;
 }
 
-/*
- * Program begins execution at this point.
- * 
- * At the moment I don't have any further  plans of adding features to it.
- * I think it is a simple enough process to not overbloat this.
-*/
 int main(int argc, char *argv[]) {
 
 	printf("Linux 64-Bit mmap based shellcode loader by Travis \"rjkall\".\n");
 	
-	/* Just check whether we have a file argument or not*/
 	if(argc == 2) {
 		parse(argv[1],0);
 	} else if (argc == 3) {
